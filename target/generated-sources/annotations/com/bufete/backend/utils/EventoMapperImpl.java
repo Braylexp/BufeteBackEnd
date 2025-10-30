7d4ed4b9122d70f86508b65e89dc43b879309637
Braylexp <devbraylex@gmail.com>
2025-10-30 08:52:42 -0500

    compilation 1

--- target/generated-sources/annotations/com/bufete/backend/utils/EventoMapperImpl.java
+++ target/generated-sources/annotations/com/bufete/backend/utils/EventoMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-30T08:44:36-0500",
+    date = "2025-10-30T08:51:41-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
 )
 @Component
